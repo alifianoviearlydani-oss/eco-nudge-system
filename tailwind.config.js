838960dafada0a63e77b3332ab5350f5a96a39b3
alifianoviearlydani-oss <alifianoviearlydani@gmail.com>
2026-05-21 21:20:42 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        // Kita daftarkan palet warna ijo hutan pilihanmu di sini biar bisa dipanggil otomatis
-        forest: {
-          dark: '#1F3E2E',
-          medium: '#29513C',
-          light: '#3D795A',
-          accent: '#60AF86',
-          soft: '#D7EBE1',
-        }
-      }
-    },
+    extend: {},
   },
   plugins: [],
-}
+};
